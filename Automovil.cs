46ad6452593ab009d64b41ce1102c1d7fafdbb11
“Michellediaz23 <ml7992066@gmail.com>
2024-05-13 03:40:51 +0200

    mi prueba

---
+++ Automovil.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Automovil
+    {
+        internal void ReabastecerCombustible(object capacidadTanqueCombustible)
+        {
+            throw new NotImplementedException();
+        }
+
+        public partial class Automovil
+    {
+        public string Marca { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public double CapacidadTanqueCombustible { get; set; }
+        public double NivelCombustible { get; private set; }
+        public double RendimientoCombustible { get; set; }
+
+        public Automovil(string marca, string placa, string modelo, string color, double capacidadTanqueCombustible, double rendimientoCombustible)
+        {
+            Marca = marca;
+            Placa = placa;
+            Modelo = modelo;
+            Color = color;
+            CapacidadTanqueCombustible = capacidadTanqueCombustible;
+            NivelCombustible = 0;
+            RendimientoCombustible = rendimientoCombustible;
+        }
+
+            public Automovil(string marca, string placa, string modelo, string color, double capacidadTanqueCombustible, double nivelCombustible, double rendimientoCombustible) : this(marca, placa, modelo, color, capacidadTanqueCombustible, nivelCombustible)
+            {
+                RendimientoCombustible = rendimientoCombustible;
+            }
+
+            public bool PuedeConducir(int distancia)
+        {
+            return (NivelCombustible * RendimientoCombustible) >= distancia;
+        }
+
+        public void Conducir(int distancia)
+        {
+            if (!PuedeConducir(distancia))
+            {
+                throw new InvalidOperationException("No hay suficiente combustible para recorrer la distancia especificada.");
+            }
+
+            double consumoCombustible = (double)distancia / RendimientoCombustible;
+            NivelCombustible -= consumoCombustible;
+        }
+
+        public void ReabastecerCombustible(double cantidad)
+        {
+            if (NivelCombustible + cantidad > CapacidadTanqueCombustible)
+            {
+                throw new InvalidOperationException("La cantidad de combustible a reabastecer supera la capacidad máxima del tanque.");
+            }
+
+            NivelCombustible += cantidad;
+        }
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static ConsoleApp5.Automovil;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Automovil toyota = new Automovil("Toyota", "ABC123", "Corolla", "Rojo", 50, 10);
+        Automovil honda = new Automovil("Honda", "DEF456", "Civic", "Azul", 40, 12);
+
+        Console.WriteLine("Toyota:");
+        Console.WriteLine($"  Puede conducir 100 km: {toyota.PuedeConducir(100)}");
+        toyota.Conducir(50);
+        Console.WriteLine($"  Nivel de combustible: {toyota.NivelCombustible} galones");
+        toyota.ReabastecerCombustible(20);
+        Console.WriteLine($"  Nivel de combustible: {toyota.NivelCombustible} galones");
+
+        Console.WriteLine("Honda:");
+        Console.WriteLine($"  Puede conducir 150 km: {honda.PuedeConducir(150)}");
+        honda.Conducir(100);
+        Console.WriteLine($"  Nivel de combustible: {honda.NivelCombustible} galones");
+        honda.ReabastecerCombustible(30);
+        Console.WriteLine($"  Nivel de combustible: {honda.NivelCombustible} galones");
+    }
+}
